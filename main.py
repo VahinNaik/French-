7f23fd7472ff8e10a1415a686a86ac0e2d052306
thelazerpotaos <81989499+VahinNaik@users.noreply.github.com>
2021-11-03 01:18:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,116 @@
+import random
+
+for i in range (28):
+  a = random.randint( 1 , 14 ) 
+  if a==1:
+    print("eu")
+    i = str.lower(input(" what is the engish? "))
+    if i==("had"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: had" )
+  elif a==2:
+    print("été")
+    i = str.lower(input(" what is the english? "))
+    if i==("was"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: was" )
+  elif a==3:
+    print("dû")
+    i = str.lower(input(" what is the english? "))
+    if i==("had to"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: had to" )
+  elif a==4:
+    print("dit")
+    i = str.lower(input(" what is the english? "))
+    if i==("said"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: said" )
+  elif a==5:
+    print("écrit")
+    i = str.lower(input(" what is the english? "))
+    if i==("wrote"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: wrote" )
+  elif a==6:
+    print("fait")
+    i = str.lower(input(" what is the english? "))
+    if i==("did/made"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: did/made" )
+  elif a==7:
+    print("lu")
+    i = str.lower(input(" what is the english? "))
+    if i==("read"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: read" )
+  elif a==8:
+    print("mis")
+    i = str.lower(input(" what is the english? "))
+    if i==("put"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: put" )
+  elif a==9:
+    print("pu")
+    i = str.lower(input(" what is the english? "))
+    if i==("could"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: could" )
+  elif a==10:
+    print("pris")
+    i = str.lower(input(" what is the english? "))
+    if i==("took"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: took" )
+  elif a==11:
+    print("compris")
+    i = str.lower(input(" what is the english? "))
+    if i==("understood"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: understood" )
+  elif a==12:
+    print("appris")
+    i = str.lower(input(" what is the english? "))
+    if i==("learned"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: learned" )
+  elif a==13:
+    print("vu")
+    i = str.lower(input(" what is the english? "))
+    if i==("saw"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: saw" )
+  elif a==14:
+    print("voulu")
+    i = str.lower(input(" what is the english? "))
+    if i==("wanted"):
+      print("correct!")
+    else:
+      print("incorrect")
+      print( "the correct answer is: wanted" )
